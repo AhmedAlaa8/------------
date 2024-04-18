2567f33457f2d47b945d301ebfbafbad31664034
AhmedAlaaAbd <aaahhmed43@gmail.com>
2024-04-18 16:04:50 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -12,74 +12,72 @@ $(document).ready(function () {
 	 *
 	 */
 
-	let b = (class_place, text, index, intrv) => {
+	// let b = (class_place, text, index, intrv) => {
 
-		$(class_place).append(text[index++])
+	// 	$(class_place).append(text[index++])
 
-		let vv = setTimeout(() => {
+	// 	let vv = setTimeout(() => {
 
-			b(class_place, text, index, intrv)
+	// 		b(class_place, text, index, intrv)
 
-		}, intrv);
+	// 	}, intrv);
 
-		if (text.length == index + 1) {
+	// 	if (text.length == index + 1) {
 
-			clearTimeout(vv);
+	// 		clearTimeout(vv);
 
-		}
-
-	};
-
-	b(".Sub-address", "نحن فرييق متكامل الهدف الاول والاساسي هوا راحه العميل ورضائه ", 0, 40)
-
-
-
-	// (function ($) {
-
-
-
-	// 	$.fn.parallax = function (options) {
-
-	// 		var windowHeight = $(window).height();
-
-	// 		// Establish default settings
-	// 		var settings = $.extend({
-	// 			speed: 0.15
-	// 		}, options);
-
-	// 		// Iterate over each object in collection
-	// 		return this.each(function () {
-
-	// 			// Save a reference to the element
-	// 			var $this = $(this);
-
-	// 			// Set up Scroll Handler
-	// 			$(document).scroll(function () {
-
-	// 				var scrollTop = $(window).scrollTop();
-	// 				var offset = $this.offset().top;
-	// 				var height = $this.outerHeight();
-
-	// 				// Check if above or below viewport
-	// 				if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-	// 					return;
-	// 				}
-
-	// 				var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
-
-	// 				// Apply the Y Background Position to Set the Parallax Effect
-	// 				$this.css('background-position', 'center ' + yBgPosition + 'px');
-
-	// 			});
-	// 		});
 	// 	}
-	// }(jQuery));
+
+	// };
+
+	// b(".Sub-address", "نحن فرييق متكامل الهدف الاول والاساسي هوا راحه العميل ورضائه ", 0, 40)
+
+
+
+	(function ($) {
+
+		$.fn.parallax = function (options) {
+
+			var windowHeight = $(window).height();
+
+			// Establish default settings
+			var settings = $.extend({
+				speed: 0.15
+			}, options);
+
+			// Iterate over each object in collection
+			return this.each(function () {
+
+				// Save a reference to the element
+				var $this = $(this);
+
+				// Set up Scroll Handler
+				$(document).scroll(function () {
+
+					var scrollTop = $(window).scrollTop();
+					var offset = $this.offset().top;
+					var height = $this.outerHeight();
+
+					// Check if above or below viewport
+					if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+						return;
+					}
+
+					var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+
+					// Apply the Y Background Position to Set the Parallax Effect
+					$this.css('background-position', 'center ' + yBgPosition + 'px');
+
+				});
+			});
+		}
+	}(jQuery));
 
 	// Loader
-	// $(window).load(function () {
-	// 	$(".loader-overlay").fadeOut("slow");
-	// })
-	$(".loader-overlay").fadeOut("slow");
+	$(window).load(function () {
+		$(".loader-overlay").fadeOut("slow");
+	})
+	// $(".loader-overlay").fadeOut("slow");
 
 	//Counter
 	$('.counter').counterUp({
